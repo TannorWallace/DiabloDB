877b272d151fc17214ff0bb661f625c71900ff8a
TannorWallace <tannor.wallace@gmail.com>
2019-11-06 11:32:28 -0700

    Ok now lets connect to the GHDB

---
+++ Models/OtherDieties.cs
@@ -0,0 +1,10 @@
+namespace DiabloDB.Models
+{
+  public class OtherDieties
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Story { get; set; }
+
+  }
+}
